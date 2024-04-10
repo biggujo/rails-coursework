62d1dc2308b6194d58c9f804695b81ba5438634e
Dmytro Ivashchenko <gen.style22@gmail.com>
2024-04-10 16:01:00 +0300

    feat: complete profile edit (without password)

--- frontend/src/interfaces/User.interface.ts
+++ frontend/src/interfaces/User.interface.ts
@@ -1,6 +1,8 @@
 interface User {
   email: string;
   nickname: string;
+  created_at: string;
+  updated_at: string;
 }
 
 export default User;
--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,31 +1,47 @@
-import { useAuth } from '../providers';
-import { Box, Button, Container, Tab, Typography } from '@mui/material';
+import { Box, Container, Tab, Typography } from '@mui/material';
 import useGetProfileQuery from '../hooks/query/useGetProfileQuery.ts';
-import { Outlet, useNavigate } from 'react-router-dom';
 import useCheckSessionExpiration from '../hooks/useCheckSessionExpiration.tsx';
 import { AxiosError } from 'axios';
 import { TabContext, TabList, TabPanel } from '@mui/lab';
-import { SyntheticEvent, useState } from 'react';
+import { ReactElement, ReactNode, useState } from 'react';
 import ProfileUpdateForm from '../components/ProfileUpdateForm';
+import { User } from '../interfaces';
+import DateConverters from '../utils/date-converters.ts';
 
-const ProfileMainData = () => {
-  const profileQuery = useGetProfileQuery();
+const ProfileMainData = ({ userData }: { userData: Partial<User, 'email' | 'nickname'> }) => {
+  return <ul>
+    <li>
+      <Typography>Email: <b>{userData.email}</b></Typography>
+    </li>
+    <li>
+      <Typography>Nickname: <b>{userData.nickname}</b></Typography>
+    </li>
+  </ul>;
+};
 
-  return <>{profileQuery.isError && <Typography>{(profileQuery.error as AxiosError).response!.statusText}</Typography>}
-    {profileQuery.isSuccess && <ul>
-      <li>
-        <Typography>Email: <b>{profileQuery.data.data.email}</b></Typography>
-      </li>
-      <li>
-        <Typography>Nickname: <b>{profileQuery.data.data.nickname}</b></Typography>
-      </li>
-    </ul>}</>;
+const ProfileUtilityData = ({ userData }: { userData: Partial<User, 'created_at' | 'updated_at'> }) => {
+  return <ul>
+    <li>
+      <Typography>Created at: <b>{String(DateConverters.extractReadable(userData.created_at))}</b></Typography>
+    </li>
+    <li>
+      <Typography>Updated at: <b>{String(DateConverters.extractReadable(userData.updated_at))}</b></Typography>
+    </li>
+  </ul>;
+};
+
+const getSubtitle = (text: string) => {
+  return <Typography variant={'h4'}
+                     component={'h3'}
+                     marginBottom={2}>
+    {text}
+  </Typography>;
 };
 
 export default function ProfilePage() {
-  const profileQuery = useGetProfileQuery({ enabled: false });
-  useCheckSessionExpiration(profileQuery.error);
   const [pageValue, setPageValue] = useState<'main' | 'settings'>('main');
+  const profileQuery = useGetProfileQuery();
+  useCheckSessionExpiration(profileQuery.error);
 
   const handleChange = (_, newValue: string) => {
     setPageValue(newValue);
@@ -38,18 +54,22 @@ export default function ProfilePage() {
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <TabList onChange={handleChange}>
             <Tab label="Main information" value="main" />
-            <Tab label="Change profile" value="settings" />
+            <Tab label="Change data" value="settings" />
           </TabList>
         </Box>
         <TabPanel value="main">
-          <ProfileMainData />
+          {profileQuery.isLoading && <Typography>Loading...</Typography>}
+          {profileQuery.isError && <Typography>{(profileQuery.error as AxiosError).response!.statusText}</Typography>}
+          {profileQuery.isSuccess && <>
+            {getSubtitle('Main data')}
+            <ProfileMainData userData={profileQuery.data.data} />
+            {getSubtitle('Utility data')}
+            <ProfileUtilityData userData={profileQuery.data.data} />
+          </>}
+
         </TabPanel>
         <TabPanel value="settings">
-          <Typography variant={'h4'}
-                      component={'h3'}
-                      marginBottom={2}>
-            Update profile
-          </Typography>
+          {getSubtitle('Update profile')}
           <ProfileUpdateForm />
         </TabPanel>
       </TabContext>
