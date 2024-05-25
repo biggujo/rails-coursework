1c9e52104732c46bcdfde3580e220ca41a7a21c3
nottworry1 <kupchenkoanastasiia@gmail.com>
2024-05-25 18:44:01 +0200

    feat: add friend system

---
+++ backend/app/controllers/friends_controller.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+class FriendsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authorize_friends_manage!, only: %i[create destroy]
+
+  def create
+    return render_error("Can't add yourself as a friend") if same_user?
+
+    user = User.find(params[:user_id])
+    friend = User.find(params[:friend_id])
+
+    return render_error("Already added as a friend") if user.friends.include?(friend)
+
+    user.friends << friend
+    render json: {success: "Friend added successfully"}, status: :created
+  rescue ActiveRecord::RecordNotFound
+    render_error("User not found")
+  end
+
+  def destroy
+    user = User.find(params[:user_id])
+    friend = User.find(params[:id])
+
+    return render_error("Friend not found") unless user.friends.include?(friend)
+
+    user.remove_friend(friend)
+    render json: {success: "Friend removed successfully"}, status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render_error("User not found")
+  end
+
+  def mutual_friends
+    user = User.find(params[:user_id])
+    render json: user.friends, status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render json: {error: "User not found"}, status: :not_found
+  end
+
+  def followers
+    user = User.find(params[:user_id])
+    render json: user.followers, status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render json: {error: "User not found"}, status: :not_found
+  end
+
+  def following
+    user = User.find(params[:user_id])
+    render json: user.following, status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render json: {error: "User not found"}, status: :not_found
+  end
+
+  private
+
+  def same_user?
+    params[:user_id].to_s.eql? params[:friend_id].to_s
+  end
+
+  def render_error(message)
+    render json: {error: message}, status: :unprocessable_entity
+  end
+
+  def authorize_friends_manage!
+    return if params[:user_id].to_s == current_user.id.to_s
+
+    raise CanCan::AccessDenied
+  end
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_22_132254) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_25_143035) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friends", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.index ["friend_id"], name: "index_friends_on_friend_id"
+    t.index ["user_id", "friend_id"], name: "index_friends_on_user_id_and_friend_id", unique: true
+    t.index ["user_id"], name: "index_friends_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
