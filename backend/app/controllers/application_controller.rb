25ec0c8c8487117e0871451e16a2419bc8e1b39e
Dmytro Ivashchenko <gen.style22@gmail.com>
2024-05-26 17:12:21 +0300

    feat: last seen at on backend

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::API
   before_action :configure_devise_params, if: :devise_controller?
+  before_action :update_last_seen_at, if: -> { user_signed_in? && current_user.last_seen_at < 10.seconds.ago }
 
   include Pagy::Backend
+
+  def update_last_seen_at
+    current_user.update_column(:last_seen_at, Time.now)
+  end
 
   def configure_devise_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :nickname])
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_26_121027) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_26_135818) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_26_121027) do
     t.datetime "updated_at", null: false
     t.string "jti", null: false
     t.string "nickname", null: false
+    t.datetime "last_seen_at", default: "2024-05-26 14:02:10", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
