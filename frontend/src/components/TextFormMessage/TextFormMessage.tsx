ccdc4db83d78e47de1006a5c84a5b2e6927bac5e
Dmytro Ivashchenko <gen.style22@gmail.com>
2024-05-26 15:46:17 +0300

    feat: max message length on frontend

--- frontend/src/components/TextFormMessage/TextFormMessage.tsx
+++ frontend/src/components/TextFormMessage/TextFormMessage.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, TextField } from '@mui/material';
+import { Button, Grid, TextField, Typography } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import useTextMessageForm from '../../hooks/forms/useTextMessageForm.ts';
 import { FormikProvider } from 'formik';
@@ -9,7 +9,7 @@ interface Props {
 }
 
 export default function TextFormMessage({ onSubmit }: Props) {
-  const formik = useTextMessageForm(onSubmit);
+  const [formik, maxLength] = useTextMessageForm(onSubmit);
 
   return (
     <FormikProvider value={formik}>
@@ -26,8 +26,33 @@ export default function TextFormMessage({ onSubmit }: Props) {
               label={'Message'}
               autoComplete={'off'}
               variant={'standard'}
+              inputProps={{
+                maxLength,
+              }}
               fullWidth
             />
+          </Grid>
+          <Grid
+            item
+            sx={{
+              height: '100%',
+              width: '80px',
+            }}
+          >
+            <Grid container justifyContent={'center'}>
+              <Grid item>
+                <Typography
+                  color={
+                    formik.values.message.length === maxLength ? 'red' : 'black'
+                  }
+                  sx={{
+                    mb: 0.75,
+                  }}
+                >
+                  {formik.values.message.length}/{maxLength}
+                </Typography>
+              </Grid>
+            </Grid>
           </Grid>
           <Grid item>
             <Button
--- frontend/src/hooks/forms/useTextMessageForm.ts
+++ frontend/src/hooks/forms/useTextMessageForm.ts
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
 import { useFormik } from 'formik';
 
+const MAX_MESSAGE_LENGTH = 500;
+
 const validationSchema = Yup.object({
-  message: Yup.string().trim().required(),
+  message: Yup.string().trim().max(MAX_MESSAGE_LENGTH).required(),
 });
 
 const useTextMessageForm = (onSubmit: (message: string) => void) => {
@@ -12,14 +14,17 @@ const useTextMessageForm = (onSubmit: (message: string) => void) => {
     message: '',
   };
 
-  return useFormik({
+  const formik = useFormik({
     initialValues,
     validationSchema,
+    validateOnChange: true,
     onSubmit: (values, { resetForm }) => {
       onSubmit(values.message);
 
       resetForm();
     },
   });
+
+  return [formik, MAX_MESSAGE_LENGTH];
 };
 export default useTextMessageForm;
