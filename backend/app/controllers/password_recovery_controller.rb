d2e8b5d946b2b20105abc08266398944d7079289
nottworry1 <144688011+nottworry1@users.noreply.github.com>
2024-05-27 08:56:32 +0200

    feat: password recovery api (#20)

---
+++ backend/app/controllers/password_recovery_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class PasswordRecoveryController < ApplicationController
+  def reset
+    user = User.find_by(email: password_params[:email])
+
+    if user.present?
+      user.send_reset_password_instructions
+      render json: { message: 'Password recovery instructions have been sent to your email.' }, status: :ok
+    else
+      render json: { error: 'Email not found.' }, status: :not_found
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:email)
+  end
+end
