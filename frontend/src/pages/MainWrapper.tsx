3338f4c02dfaad728e2dbb150f6bed95fce44e79
Dmytro Ivashchenko <gen.style22@gmail.com>
2024-06-02 13:23:12 +0300

    feat: scroll to top button

--- frontend/src/pages/MainWrapper.tsx
+++ frontend/src/pages/MainWrapper.tsx
@@ -1,10 +1,12 @@
 import NavBar from '../components/NavBar';
-import { Container, Stack } from '@mui/material';
+import { Box, Container, Stack, useTheme } from '@mui/material';
 import SideBar from '../components/SideBar';
 import { ReactElement } from 'react';
 import { useSelector } from 'react-redux';
 import { selectAuthIsLoggedIn } from '../redux/auth/selectors.ts';
 import Footer from '../components/Footer/Footer.tsx';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import ScrollToTop from 'react-scroll-to-top';
 
 interface Props {
   children: ReactElement;
@@ -12,6 +14,7 @@ interface Props {
 
 export default function MainWrapper({ children }: Props) {
   const isLoggedIn = useSelector(selectAuthIsLoggedIn);
+  const theme = useTheme();
 
   return (
     <>
@@ -30,6 +33,23 @@ export default function MainWrapper({ children }: Props) {
         </Stack>
       </Container>
       {isLoggedIn && <Footer />}
+      <ScrollToTop
+        smooth
+        style={{
+          borderRadius: '50%',
+          border: `1px solid ${theme.palette.primary.main}`,
+          boxShadow: 'none',
+        }}
+        component={
+          <Box
+            sx={{
+              color: theme.palette.primary.main,
+            }}
+          >
+            <KeyboardArrowUpIcon />
+          </Box>
+        }
+      />
     </>
   );
 }
